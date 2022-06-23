f7d740b354aa4ffef2349572203253ed4bb88269
Jonathan Salas <jonathansalas1122@gmail.com>
2022-06-23 09:38:48 -0500

    create utils folder and firebase js file in utils forlder that has firebase config data

---
+++ src/utils/firebase/firebase.util.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2U2xJ1yphk2l1XyF8c1rHGtU8GKJbeVI",
+  authDomain: "ztm-crwn-clothing-db-67b9f.firebaseapp.com",
+  projectId: "ztm-crwn-clothing-db-67b9f",
+  storageBucket: "ztm-crwn-clothing-db-67b9f.appspot.com",
+  messagingSenderId: "193180632409",
+  appId: "1:193180632409:web:d2481c255ae62de21c7848"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
